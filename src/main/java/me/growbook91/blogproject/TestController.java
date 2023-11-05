8358c1424abae391f10a3b3c53c3e09b806b3548
growbook91 <70497021+growbook91@users.noreply.github.com>
2023-11-05 23:39:59 +0900

    Initial commit

---
+++ src/main/java/me/growbook91/blogproject/TestController.java
@@ -0,0 +1,12 @@
+package me.growbook91.blogproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello, World";
+    }
+}
