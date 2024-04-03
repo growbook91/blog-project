7be7276828cd084edd145184da9c855e4c63480a
kangmin lee <growbook91@gmail.com>
2024-04-03 16:17:37 +0900

    chapter 7

--- src/main/java/me/growbook91/blogproject/dto/ArticleViewResponse.java
+++ src/main/java/me/growbook91/blogproject/dto/ArticleViewResponse.java
@@ -1,9 +1,13 @@
 package me.growbook91.blogproject.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import me.growbook91.blogproject.domain.Article;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
 public class ArticleViewResponse {
     private Long id;
     private String title;
